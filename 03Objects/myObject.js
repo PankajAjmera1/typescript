0670802da0b4d3ecb2ae713539c76ffecfaa9acd
PankajAjmera1 <coderajmera@gmail.com>
2025-09-07 22:25:30 +0530

    Objects

---
+++ 03Objects/myObject.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var User = {
+    name: "Pankaj",
+    email: "pankaj@eg.com",
+    isActive: true
+};
+function createUser(_a) {
+    var string = _a.name, boolean = _a.isPaid;
+}
+createUser({ name: "Pankaj", isPaid: false });
+function createCourse() {
+    return { name: "pankaj", isPaid: false };
+}
---
+++ 03Objects/myObject.ts
@@ -0,0 +1,34 @@
+const User = {
+    name: "Pankaj",
+    email: "pankaj@eg.com",
+    isActive: true
+}
+
+function createUser({name: string, isPaid: boolean}){
+
+}
+createUser({name: "Pankaj", isPaid: false})
+
+
+
+
+function createCourse(): {name: string, isPaid: boolean} {
+    return {name: "pankaj",isPaid: false}
+}
+
+
+//type aliases
+type User ={
+    name: string;
+    email: string;
+    isActive: boolean
+}
+function createUser1(user: User){
+    return {
+        name: "",
+        email: "",
+        isActive: true
+    }
+}
+createUser1({name: "", email: "",isActive: true})
+export{}
