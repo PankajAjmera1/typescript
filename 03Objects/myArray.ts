aab46d556312964e5f343bf6ad7d699ef2a25dd9
PankajAjmera1 <coderajmera@gmail.com>
2025-09-09 22:30:15 +0530

    Basic Array

---
+++ 03Objects/myArray.ts
@@ -0,0 +1,22 @@
+const superHeros: string[] = [] 
+const heroPower: Array<number> = []
+
+type User ={
+    name: string
+    isActive: boolean
+}
+
+const allUsers: User[] = []
+
+allUsers.push({
+    name: "Pankaj",
+    isActive: true
+})
+
+superHeros.push("Spiderman")
+
+
+const MLModels: number[][] = [
+    [255, 255, 255],
+    [180, 180, 180]
+]
