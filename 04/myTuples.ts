adc0b65d7e793aba4637e7d24449f6ab43d72fc2
PankajAjmera1 <coderajmera@gmail.com>
2025-09-09 23:11:00 +0530

    Tuples

---
+++ 04/myTuples.ts
@@ -0,0 +1,15 @@
+// const user: string[] = ["pankaj"]
+//maintain of order
+const user: [string, number, boolean]  = [ "pankaj", 131, true]
+
+let rgb: [number, number, number] = [255, 255, 255]
+
+type user1 = [number, string]
+
+const newUser: user1 = [112, "pa@gmail.com"]
+//changing value issue
+newUser[1] = "pankaj"
+// newUser.push(true)
+
+
+
