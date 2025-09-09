59acf3b5a415aa6c9a004448f37d35a3494804c2
PankajAjmera1 <coderajmera@gmail.com>
2025-09-09 23:52:22 +0530

    Enums

---
+++ 04/myEnums.ts
@@ -0,0 +1,15 @@
+// const AISLE = 0
+// const MIDDLE = 1
+// const WINDOW = 2
+
+enum SeatChoice {
+    AISLE = 10,
+    MIDDLE,
+    WINDOW
+}
+const seat = SeatChoice.WINDOW
+ enum SeatChoice2 {
+    AISLE = "aisle",
+    MIDDLE =1,
+    WINDOWS
+}
